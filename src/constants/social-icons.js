7fc63b3781f6f697a6791d602608262a5899bc51
juamecos <juamecos@gmail.com>
2020-05-13 13:00:15 +0200

    daploy contentful still not working

--- src/constants/social-icons.js
+++ src/constants/social-icons.js
@@ -1,13 +1,13 @@
 import React from "react"
-import { FaFacebook, FaInstagramSquare} from "react-icons/fa"
+import { FaFacebook, FaInstagramSquare } from "react-icons/fa"
 
 export default [
-  {
-    icon: <FaFacebook />,
-    url: "https://www.facebook.com/miquel.masmartinez",
-  },
-  {
-    icon: <FaInstagramSquare />,
-    url: "https://www.instagram.com/mikimas79/",
-  }
+    {
+        icon: <FaFacebook />,
+        url: "https://www.facebook.com/miquel.masmartinez",
+    },
+    {
+        icon: <FaInstagramSquare />,
+        url: "https://www.instagram.com/mikimas79/",
+    },
 ]
